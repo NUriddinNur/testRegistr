b29345ed65c50e1de18ec179f8022efaf071259b
NurPro <nurik-31@mail.ru>
2022-09-22 04:28:34 +0500

    test2

--- src/utils/db.js
+++ src/utils/db.js
@@ -6,7 +6,7 @@ dotenv.config()
 const sequelize = new Sequelize({
     dialect: 'postgres',
     username: process.env.PG_USER,
-    password: process.env.PG_PASSWORD + '',
+    password: "1234",
     database: process.env.PG_DATABASE,
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
