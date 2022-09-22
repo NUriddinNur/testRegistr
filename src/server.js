6151b5f0258b1d6320dc6d94879876afd715cdc4
NurPro <nurik-31@mail.ru>
2022-09-22 15:37:35 +0500

    test11

--- src/server.js
+++ src/server.js
@@ -13,13 +13,16 @@ const PORT = process.env.PORT || 4006
 const app = express()
 
 
+
 const corsOptions ={
     origin:'*', 
     credentials:true,          
     optionSuccessStatus:200,
- }
- 
- app.use(cors(corsOptions))
+}
+
+
+console.log(corsOptions);
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use('/api', validation)
